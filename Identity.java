fd73a8f09b4bfc989dfeca12c2ab542dee7adb95
Shashankkb7 <113162430+Shashankkb7@users.noreply.github.com>
2022-09-14 19:12:15 +0530

    Add files via upload

---
+++ Identity.java
@@ -0,0 +1,71 @@
+class Identity{
+	static void printName(String name)
+	{
+		System.out.println("entered printName");
+		System.out.println("Name entered:"+ name);
+	}
+	static void printNameAndPhone(String name,long Phone)
+	{
+		System.out.println("entered printNameAndPhone");
+		System.out.println("Name"+ name);
+		System.out.println("Phone"+ Phone);
+	}
+	static void printNameAndEmail(String name,String email)
+	{
+		System.out.println("entered printNameAndEmail");
+		System.out.println("Name"+ name);
+		System.out.println("Email"+ email);
+	}
+	static void printNameAndAge(String name,int age)
+	{
+		System.out.println("entered printNameAndAge");
+		System.out.println("Name"+ name);
+		System.out.println("Age"+ age);
+	}
+	static void printNameAndAddress(String name,String address)
+	{
+		System.out.println("entered printNameAndAddress");
+		System.out.println("Name"+ name);
+		System.out.println("Address"+ address);
+	}
+	static void printNameAndEmailAndAddressAndAge(String name,String email,String address,int age)
+	{
+		System.out.println("entered printNameAndEmailAndAddressAndAge");
+		System.out.println("Name"+ name);
+		System.out.println("Email"+ email);
+		System.out.println("Address"+ address);
+		System.out.println("Age"+ age);
+	}
+	static void printNameAndShirtSize(String name,int ShirtSize)
+	{
+		System.out.println("entered printNameAndShirtSize");
+		System.out.println("Name"+ name);
+		System.out.println("ShirtSize"+ ShirtSize);
+	}
+	static void printNameAndGender(String name,char Gender)
+	{
+		System.out.println("entered  printNameAndGender");
+		System.out.println("Name"+ name);
+		System.out.println("Gender"+ Gender);
+	}
+	static void printNameAndWorking(String name,String Working)
+	{
+		System.out.println("entered  printNameAndWorking");
+		System.out.println("Name"+ name);
+		System.out.println("Working"+ Working);
+	}
+	static void printNameAndPantSize(String name,int PantSize)
+	{
+		System.out.println("entered printNameAndPantSize");
+		System.out.println("Name"+ name);
+		System.out.println("PantSize"+ PantSize);
+	}
+	static void printNameAndHeightAndWeight(String name,float Height,int Weight)
+	{
+		System.out.println("entered printNameAndHeightAndWeight");
+		System.out.println("Name"+ name);
+		System.out.println("Height"+ Height);
+		System.out.println("Weight"+ Weight);
+	}
+}
+	
---
+++ IdentityRunner.java
@@ -0,0 +1,158 @@
+class IdentityRunner{
+	public static void main(String[] names)
+	{
+		System.out.println("entered main...");
+	
+		Identity.printName("Darshan");
+		Identity.printName("Varsha");
+		Identity.printName("Sujan");
+		Identity.printName("Spoorthi");
+		Identity.printName("Shwetha");
+		Identity.printName("Arpitha");
+		Identity.printName("Geetha");
+		Identity.printName("Suresh");
+		Identity.printName("Shashikumar");
+		Identity.printName("Shamitha");
+		Identity.printName("Harshitha");
+		Identity.printName("Varun");
+		Identity.printName("Sneha");
+		
+		Identity.printNameAndPhone("Darshan",6362420642L);
+		Identity.printNameAndPhone("Sharath",9380312452L);
+		Identity.printNameAndPhone("Varsha",6342269852L);
+		Identity.printNameAndPhone("Sujan",9108120838L);
+		Identity.printNameAndPhone("Spoorthi",8431675593L);
+		Identity.printNameAndPhone("Suresh",8663401255L);
+		Identity.printNameAndPhone("Arpitha",9686021910L);
+		Identity.printNameAndPhone("Geetha",8197988229L);
+		Identity.printNameAndPhone("Shamitha",7760968583L);
+		Identity.printNameAndPhone("Harshitha",9980360358L);
+		Identity.printNameAndPhone("Shasikumar",9731064686L);
+		Identity.printNameAndPhone("Krupa",9164575234L);
+		Identity.printNameAndPhone("leelavathi",9481418835L);
+		
+		Identity.printNameAndEmail("Sharath","Sharath.xworkz@gmail.com");
+		Identity.printNameAndEmail("Darshan","Darshan.xworkz@gmail.com");
+		Identity.printNameAndEmail("Varsha","Varsha.xworkz@gmail.com");
+		Identity.printNameAndEmail("Sujan","Sujan.xworkz@gmail.com");
+		Identity.printNameAndEmail("Spoorthi","Spoorthi.xworkz@gmail.com");
+		Identity.printNameAndEmail("Shwetha","Shwetha.xworkz@gmail.com");
+		Identity.printNameAndEmail("Suresh","Suresh.xworkz@gmail.com");
+		Identity.printNameAndEmail("Arpitha","Arpitha.xworkz@gmail.com");
+		Identity.printNameAndEmail("Geetha","Geetha.xworkz@gmail.com");
+		Identity.printNameAndEmail("Shashikumar","Shashikumar.xworkz@gmail.com");
+		Identity.printNameAndEmail("Shamitha","Shamitha.xworkz@gmail.com");
+		Identity.printNameAndEmail("Harshitha","Harshitha.xworkz@gmail.com");
+		
+		Identity.printNameAndAge("Darshan",32);
+		Identity.printNameAndAge("Sharath",22);
+		Identity.printNameAndAge("Varsha",18);
+		Identity.printNameAndAge("Sujan",17);
+		Identity.printNameAndAge("Spoorthi",21);
+		Identity.printNameAndAge("Suresh",52);
+		Identity.printNameAndAge("Arpitha",16);
+		Identity.printNameAndAge("Geetha",42);
+		Identity.printNameAndAge("Shamitha",12);
+		Identity.printNameAndAge("Harshitha",20);
+		Identity.printNameAndAge("Shasikumar",48);
+		Identity.printNameAndAge("Krupa",32);
+		Identity.printNameAndAge("leelavathi",66);
+		
+		Identity.printNameAndAddress("Varsha","Basavangudi Shimoga");
+		Identity.printNameAndAddress("Suresh","WhiteField Shimoga");
+		Identity.printNameAndAddress("Sujan","Courtcircle Shimoga");
+		Identity.printNameAndAddress("Geetha","RavindraNagar Shimoga");
+		Identity.printNameAndAddress("Harshitha","mysore");
+		Identity.printNameAndAddress("Shashikumar","chilur Shimoga");
+		Identity.printNameAndAddress("Krupa","Basavangudi Banglore");
+		Identity.printNameAndAddress("Leelavathi","Vinobhanagar Shimoga");
+		Identity.printNameAndAddress("Arpitha","Navale Shimoga");
+		Identity.printNameAndAddress("Shamitha","Vidhyanagar Shimoga");
+		Identity.printNameAndAddress("Shwetha","Rajajinagar Banglore");
+		Identity.printNameAndAddress("Radha","Jayanagar Banglore");
+		
+		Identity.printNameAndEmailAndAddressAndAge("Darshan","Darshan.xworkz@gmail.com","WhiteField Shimoga",31);
+		Identity.printNameAndEmailAndAddressAndAge("Sharath","Sharath.xworkz@gmail.com","WhiteField Shimoga",22);
+		Identity.printNameAndEmailAndAddressAndAge("tarun","tarun.xworkz@gmail.com","RavindraNagar Shimoga",32);
+		Identity.printNameAndEmailAndAddressAndAge("Varsha","Varsha.xworkz@gmail.com","Basavangudi Shimoga",18);
+		Identity.printNameAndEmailAndAddressAndAge("Sujan","Sujan.xworkz@gmail.com","Courtcircle Shimoga",17);
+		Identity.printNameAndEmailAndAddressAndAge("Spoorthi","Spoorthi.xworkz@gmail.com","mysore",23);
+		Identity.printNameAndEmailAndAddressAndAge("Shwetha","Shwetha.xworkz@gmail.com","Vidhyanagar Shimoga",22);
+		Identity.printNameAndEmailAndAddressAndAge("Suresh","Suresh.xworkz@gmail.com","WhiteField Shimoga",23);
+		Identity.printNameAndEmailAndAddressAndAge("Arpitha","Arpitha.xworkz@gmail.com","Navale Shimoga",19);
+		Identity.printNameAndEmailAndAddressAndAge("Geetha","Geetha.xworkz@gmail.com","Basavangudi Banglore",25);
+		Identity.printNameAndEmailAndAddressAndAge("Shashikumar","Shashikumar.xworkz@gmail.com","chilur Shimoga",22);
+		Identity.printNameAndEmailAndAddressAndAge("Shamitha","Shamitha.xworkz@gmail.com","Rajajinagar Banglore",23);
+		Identity.printNameAndEmailAndAddressAndAge("Harshitha","Harshitha.xworkz@gmail.com","Vinobhanagar Shimoga",26);
+		
+		Identity.printNameAndShirtSize("Darshan",42);
+		Identity.printNameAndShirtSize("Sharath",40);
+		Identity.printNameAndShirtSize("Varsha",36);
+		Identity.printNameAndShirtSize("Sujan",38);
+		Identity.printNameAndShirtSize("Spoorthi",36);
+		Identity.printNameAndShirtSize("Suresh",46);
+		Identity.printNameAndShirtSize("Arpitha",36);
+		Identity.printNameAndShirtSize("Geetha",42);
+		Identity.printNameAndShirtSize("Shamitha",42);
+		Identity.printNameAndShirtSize("Harshitha",40);
+		Identity.printNameAndShirtSize("Shasikumar",42);
+		Identity.printNameAndShirtSize("Krupa",38);
+		Identity.printNameAndShirtSize("leelavathi",36);
+		
+		Identity.printNameAndGender("Darshan",'M');
+		Identity.printNameAndGender("Sharath",'M');
+		Identity.printNameAndGender("Varsha",'F');
+		Identity.printNameAndGender("Sujan",'M');
+		Identity.printNameAndGender("Spoorthi",'F');
+		Identity.printNameAndGender("Suresh",'M');
+		Identity.printNameAndGender("Arpitha",'F');
+		Identity.printNameAndGender("Geetha",'F');
+		Identity.printNameAndGender("Shamitha",'F');
+		Identity.printNameAndGender("Harshitha",'F');
+		Identity.printNameAndGender("Shasikumar",'M');
+		Identity.printNameAndGender("Krupa",'F');
+		Identity.printNameAndGender("leelavathi",'F');
+		
+		Identity.printNameAndWorking("Darshan","Yes");
+		Identity.printNameAndWorking("Sharath","No");
+		Identity.printNameAndWorking("Varsha","Yes");
+		Identity.printNameAndWorking("Sujan","Yes");
+		Identity.printNameAndWorking("Spoorthi","No");
+		Identity.printNameAndWorking("Suresh","No");
+		Identity.printNameAndWorking("Arpitha","Yes");
+		Identity.printNameAndWorking("Geetha","No");
+		Identity.printNameAndWorking("Shamitha","No");
+		Identity.printNameAndWorking("Harshitha","Yes");
+		Identity.printNameAndWorking("Shasikumar","Yes");
+		Identity.printNameAndWorking("Krupa","No");
+		Identity.printNameAndWorking("leelavathi","Yes");
+		
+		Identity.printNameAndPantSize("Darshan",42);
+		Identity.printNameAndPantSize("Sharath",40);
+		Identity.printNameAndPantSize("Varsha",36);
+		Identity.printNameAndPantSize("Sujan",38);
+		Identity.printNameAndPantSize("Spoorthi",36);
+		Identity.printNameAndPantSize("Suresh",46);
+		Identity.printNameAndPantSize("Arpitha",36);
+		Identity.printNameAndPantSize("Geetha",42);
+		Identity.printNameAndPantSize("Shamitha",42);
+		Identity.printNameAndPantSize("Harshitha",40);
+		Identity.printNameAndPantSize("Shasikumar",42);
+		Identity.printNameAndPantSize("Krupa",38);
+		Identity.printNameAndPantSize("leelavathi",36);
+		
+		Identity.printNameAndHeightAndWeight("Darshan",5.5f,75);
+		Identity.printNameAndHeightAndWeight("Sharath",6.1f,60);
+		Identity.printNameAndHeightAndWeight("Varsha",5.8f,45);
+		Identity.printNameAndHeightAndWeight("Sujan",6.0f,48);
+		Identity.printNameAndHeightAndWeight("Spoorthi",4.9f,46);
+		Identity.printNameAndHeightAndWeight("Suresh",6.3f,56);
+		Identity.printNameAndHeightAndWeight("Arpitha",5.4f,46);
+		Identity.printNameAndHeightAndWeight("Geetha",5.0f,42);
+		Identity.printNameAndHeightAndWeight("Shamitha",5.8f,42);
+		Identity.printNameAndHeightAndWeight("Harshitha",5.7f,40);
+		Identity.printNameAndHeightAndWeight("Shasikumar",6.5f,72);
+		Identity.printNameAndHeightAndWeight("Krupa",4.5f,38);
+		Identity.printNameAndHeightAndWeight("leelavathi",5.4f,36);
+	}
+}
